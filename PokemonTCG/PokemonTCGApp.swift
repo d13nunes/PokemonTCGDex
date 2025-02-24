8debf7d09735bfb08c65d801edf3bd26ee5edee3
Diogo Nunes <d13nunes@gmail.com>
2025-02-24 10:57:50 +0000

    Initial Commit

---
+++ PokemonTCG/PokemonTCGApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTCGApp.swift
+//  PokemonTCG
+//
+//  Created by Diogo Nunes on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTCGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokemonTCGTests/PokemonTCGTests.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonTCGTests.swift
+//  PokemonTCGTests
+//
+//  Created by Diogo Nunes on 24/02/2025.
+//
+
+import Testing
+
+struct PokemonTCGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
