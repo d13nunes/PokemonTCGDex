bd522a29ccd4b44fcdcfe25b184eef790751ae52
Diogo Nunes <d13nunes@gmail.com>
2025-02-25 12:48:21 +0000

    improvement(API Request Cache): Add cache to improve user experience

--- PokemonTCG/Source/Networking/TCGDexAPIClient.swift
+++ PokemonTCG/Source/Networking/TCGDexAPIClient.swift
@@ -3,8 +3,21 @@ import Foundation
 
 class TCGDexAPIClient: APIClient {
   private let session: URLSession
+  private let cache: URLCache
 
-  init(session: URLSession = .shared) {
+  init(session: URLSession = .shared, cache: URLCache? = nil) {
+    if let cache = cache {
+      self.cache = cache
+    } else {
+      let memoryCapacity = 150 * 1024 * 1024  // 150MB memory cache
+      let diskCapacity = 500 * 1024 * 1024  // 500MB disk cache
+      self.cache = URLCache(
+        memoryCapacity: memoryCapacity,
+        diskCapacity: diskCapacity,
+        diskPath: nil
+      )
+    }
+    self.cache.removeAllCachedResponses()
     self.session = session
   }
 
@@ -69,11 +82,32 @@ class TCGDexAPIClient: APIClient {
     -> T
   {
     debugPrint("🔄 TCGDexAPIClient: fetchData: \(url)")
-    let (data, response) = try await session.data(from: url)
+    let request = URLRequest(url: url)
+    if let cachedResponse = cache.cachedResponse(for: request) {
+      print("🔄 TCGDexAPIClient: fetchData: \(url) - Using cached response")
+      return try decode(cachedResponse.data)
+    }
+    print("🔄 TCGDexAPIClient: fetchData: \(url) - No cached response")
+    let (data, response) = try await session.data(for: request)
+    cacheResponse(data: data, url: url)
     guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
       throw URLError(.badServerResponse)
     }
     return try decode(data)
 
+  }
+
+  private func cacheResponse(data: Data, url: URL) {
+    let cache = URLCache.shared
+    let response = CachedURLResponse(
+      response: URLResponse(
+        url: url,
+        mimeType: "application/json",
+        expectedContentLength: data.count,
+        textEncodingName: "utf-8"
+      ),
+      data: data
+    )
+    cache.storeCachedResponse(response, for: URLRequest(url: url))
   }
 }
